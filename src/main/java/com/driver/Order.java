bdbebab9591b21f3bbd9cc9008b3ce4228301f21
Gautam kumar <165561578+gautamkumar83431@users.noreply.github.com>
2025-06-26 23:01:52 +0530

    Update Order.java

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -4,24 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Order {
-	private List<Food> items;
+    private static final List<Food> orderItems = new ArrayList<>();
 
-    public Order() {
-    	// your code goes here
+    public static void addItem(Food food) {
+        orderItems.add(food);
     }
 
-    public void addItem(Food food) {
-    	// your code goes here
+    public static List<Food> getOrderItems() {
+        return orderItems;
     }
 
-    public List<Food> getItems() {
-    	// your code goes here
-        return items;
-    }
-
-    public double getTotalBill() {
-        double totalBill = 0;
-     // your code goes here
-        return totalBill;
+    public static double getTotal() {
+        return orderItems.stream().mapToDouble(Food::getPrice).sum();
     }
 }
